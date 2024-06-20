4d270f990cc5f727459cd0724c8905b39654d1af
abrar2139 <mabrarqadri3@mail.com>
2024-06-20 20:31:03 +0500

    build folder add

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Homecomponent } from './Component/home';
 import { Aboutcomponent } from './Component/about';
-
 import { Footercomponent } from './Component/footer';
 import { NavbarComponent } from './Component/navbar2';
 import Projects from './Component/projects';
-
-
 import PacmanLoader from "react-spinners/HashLoader";
 import Contact2 from './Component/contact2';
-
-
-
-
-
-
-
 const App = () => {
-      const[loading, setloading]=useState(false);
-    
+      const[loading, setloading]=useState(false); 
     useEffect(()=>{
       setloading(true)
       setTimeout(()=>{
@@ -27,38 +16,28 @@ const App = () => {
     },[])
   return (
     <div className='relative bg-gray-600'>   
-      {
-                  
+      {       
            loading?
              <div className="h-screen w-full flex justify-center items-center">
-
             <PacmanLoader
             className=' '
             color={"#F37A24"}
             loading={loading}
-            
             size={50}
             aria-label="Loading Spinner"
             data-testid="loader"
             />
-            </div>
-    
-           :
+            </div>:
            <>
            <NavbarComponent/>
-
            <Homecomponent/>
            <Aboutcomponent/>
            <Projects/>
-           
            <Contact2/>
-       
            <Footercomponent/>
            </>
       }  
-   
     </div>
   )
 }
-
 export default App
