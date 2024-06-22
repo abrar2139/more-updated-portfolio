f3442b55b003273e6cced8aa844429e47a6a6fb9
Muhammad Abrar <149916283+abrar2139@users.noreply.github.com>
2024-06-23 00:49:40 +0500

    Update navbar2.jsx

--- src/Component/navbar2.jsx
+++ src/Component/navbar2.jsx
@@ -29,7 +29,7 @@ export const NavbarComponent = () => {
             <div className={`md:flex md:items-center md:pb-0 pb-2 absolute md:static bg-amber-600 md:z-auto z-[-1] left-0 w-full md:w-auto   transition-all duration-1000  ease-in-out ${open ? 'top-12' : 'top-[-490px]'}`}>
                 
                 
-                    <ul className='text-center text-gray-700 flex items-center justify-center flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row mt-7 sm:mt-7 md:mt-0 lg:mt-0 xl:mt-0 font-semibold text-2xl'>
+                    <ul className='text-center text-gray-700 flex items-center justify-center flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row mt-7 sm:mt-7 md:mt-0 lg:mt-0 xl:mt-0 font-semibold text-[20px]'>
                     <Link to='home' smooth={true} offset={-50}  duration={900}  className="mr-0 sm:mr-0 md:mr-5 lg:mr-5 xl:mr-5 my-3 text-center sm:my-3 md:my-0 lg:my-0 xl:my-0 hover:text-gray-900 cursor-pointer ">Home</Link>
         <Link to='about' smooth={true} offset={-80}  duration={900} className="mr-0 sm:mr-0 md:mr-5 lg:mr-5 xl:mr-5 mb-3 sm:mb-3 md:mb-0 lg:mb-0 xl:mb-0  text-center hover:text-gray-900 cursor-pointer">About</Link>
         <Link to='projects' smooth={true} offset={-50}  duration={900} className="mr-0 sm:mr-0 md:mr-5 lg:mr-5 xl:mr-5 mb-3 text-center sm:mb-3 md:mb-0 lg:mb-0 xl:mb-0  hover:text-gray-900 cursor-pointer">Projects</Link>
